bcbb8705659670c0dc77751a7c1d9ab0e11f3afb
Unknown <owlsight@gmail.com>
2018-05-11 19:39:30 +0700

    add module quiz

---
+++ app/code/Icube/Quiz/Controller/Add/Index.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Icube\Quiz\Controller\Add;
+
+use \Magento\Framework\App\Action\Action;
+use \Magento\Framework\App\Action\Context;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    )
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+}
---
+++ app/code/Icube/Quiz/Model/EmployeeInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Icube\Quiz\Model;
+
+interface EmployeeInterface
+{
+	
+}
